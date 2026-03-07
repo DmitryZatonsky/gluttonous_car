b4687a7a19a160739792ad4e3646fb6a39ab3d93
Dmitry Zatonsky <_EMAIL__GITHUB>
2026-03-07 10:47:54 +0200

    изменен адрес в вайт конфиг

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gluttonous_car/',
+  base: "https://dmitryzatonsky.github.io/gluttonous_car/",
 })
